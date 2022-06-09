7c3c5b1ce148b0d0bf8f5669f7527732105c7caf
Luca Urbanowicz <human.exp11@gmail.com>
2022-06-09 16:35:54 -0600

    homepage styling complete & additions to sign up page

---
+++ client/src/components/InfoCards.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../homepg.css";
+import card6 from '../assets/card6.jpeg'
+import card3 from '../assets/card3.jpeg'
+import card4 from '../assets/card4.jpeg'
+import card13 from '../assets/card13.jpeg'
+
+export default function Info() {
+  
+
+    return (
+
+
+
+     <>
+  <li className="card" id="card_1">
+    <div className="card__content">
+      <div>
+        <h2><i>"I'm so thankful I can depend on you!"</i></h2>
+        <p>-Joan from Minneapolis </p>
+
+      </div>
+      <figure>
+        <img src={card6} alt="Image description" />
+      </figure>
+    </div>
+  </li>
+  <li className="card" id="card_2">
+    <div className="card__content">
+      <div>
+        <h2><i>"So appreciative of this service- I can finally get some errands done while I'm at work."</i></h2>
+        <p>-Brook from Scranton.</p>
+ 
+      </div>
+      <figure>
+        <img src={card3} alt="Image description" />
+      </figure>
+    </div>
+  </li>
+  <li className="card" id="card_3">
+    <div className="card__content">
+      <div>
+        <h2><i>"Thank you so much for your help- it means the world."</i></h2>
+        <p>-Addie from Austin.</p>
+  
+      </div>
+      <figure>
+        <img src={card4} alt="Image description" />
+      </figure>
+    </div>
+  </li>
+  <li className="card" id="card_4">
+    <div className="card__content">
+      <div>
+        <h2><i>"I love helping others throughout my day- I've also created amazing friendships!"</i></h2>
+        <p>-Michael from Portland</p>
+     
+      </div>
+      <figure>
+        <img src={card13} alt="Image description" />
+      </figure>
+    </div>
+  </li>
+  </>
+
+      
+  );
+  };
+
+
--- client/src/pages/home-page.js
+++ client/src/pages/home-page.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Header from '../components/Header';
 import Home from '../components/Home';
 import LoginForm from '../components/LoginForm'
-import SignUpPage from '../pages/sign-up';
+import SignUpPage from '../pages/signUpPage';
 import Footer from '../components/Footer';
 import '../homepg.css';
 
