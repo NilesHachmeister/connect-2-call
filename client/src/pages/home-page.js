bdb3185df57dfeaf40eb690c662767fef709562e
Luca Urbanowicz <human.exp11@gmail.com>
2022-06-10 18:46:14 -0600

    sign up page styling

--- client/src/pages/home-page.js
+++ client/src/pages/home-page.js
@@ -5,6 +5,7 @@ import Home from '../components/Home';
 import LoginForm from '../components/LoginForm'
 import SignUpPage from '../pages/signUpPage';
 import Footer from '../components/Footer';
+
 import '../homepg.css';
 import pattern2 from '../assets/pattern2.jpeg';
 
