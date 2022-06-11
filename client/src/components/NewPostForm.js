7bb75bfa13f303427957a2b81dfaebe230c9ae5b
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-06-11 14:02:11 -0600

    dropdown language for call language

--- client/src/components/NewPostForm.js
+++ client/src/components/NewPostForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth'
@@ -8,6 +8,8 @@ import pattern2 from '../assets/pattern2.jpeg';
 // import Auth from '../utils/auth';
 
 const NewPostForm = () => {
+
+    const currentLanguage = localStorage.getItem("i18nextLng");
     // set initial form state
     const [postFormData, setPostFormData] = useState({ taskTitle: '', callLanguage: '', description: '', callCategory: '', payment: 0, callTime: '', phoneNumberToCall: '', });
     const [addPost, { error }] = useMutation(ADD_POST);
@@ -18,12 +20,80 @@ const NewPostForm = () => {
 
     const loggedUser = Auth.getProfile();
 
+    const [taskTitleValidate, setTaskTitleValidate] = useState(false);
+    const [descriptionValidate, setDescriptionValidate] = useState(false);
+    const [callCategoryValidate, setCallCategoryValidate] = useState(false);
+    const [callTimeValidate, setCallTimeValidate] = useState(false);
+    const [phoneNumberToCallValidate, setPhoneNumberToCallValidate] = useState(false);
+
+    const [callLanguage, setCallLanguage] = useState(currentLanguage);
+
+
+
+
+
+
+
+    useEffect(() => {
+
+
+
+
+        if (callLanguage === "en") {
+            setCallLanguage("English");
+        }
+        if (callLanguage === "es") {
+            setCallLanguage("Español");
+        }
+        if (callLanguage === "ru") {
+            setCallLanguage("Русский");
+        }
+
+        console.log(currentLanguage)
+        console.log(callLanguage)
+
+
+        console.log(callLanguage)
+
+        if (postFormData.taskTitle.split("").length >= 1) {
+            setTaskTitleValidate(true)
+        } else {
+            setTaskTitleValidate(false)
+        }
+        if (postFormData.description.split("").length >= 1) {
+            setDescriptionValidate(true)
+        } else {
+            setDescriptionValidate(false)
+        }
+        if (postFormData.callCategory.split("").length >= 1) {
+            setCallCategoryValidate(true)
+        } else {
+            setCallCategoryValidate(false)
+        }
+        if (postFormData.callTime.split("").length >= 1) {
+            setCallTimeValidate(true)
+        } else {
+            setCallTimeValidate(false)
+        }
+        if (postFormData.phoneNumberToCall.split("").length >= 1) {
+            setPhoneNumberToCallValidate(true)
+        } else {
+            setPhoneNumberToCallValidate(false)
+        }
+
+    });
+
+
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setPostFormData({ ...postFormData, [name]: value });
-    };
 
+        if (name === "callLanguage") {
+            setCallLanguage(value)
+        }
+
+    };
 
     const handleFormSubmit = async (event) => {
 
@@ -90,20 +160,18 @@ const NewPostForm = () => {
                             value={postFormData.taskTitle}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>taskTitle is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>{!taskTitleValidate ? "Please enter a task title" : ""}</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label htmlFor='callLanguage'> callLanguage</Form.Label>
-                        <Form.Control
-                            type='text'
-                            placeholder='callLanguage'
-                            name='callLanguage'
-                            onChange={handleInputChange}
-                            value={postFormData.callLanguage}
-                            required
-                        />
-                        <Form.Control.Feedback type='invalid'> callLanguage is required!</Form.Control.Feedback>
+                        <Form.Label htmlFor='callLanguage' onChange={handleInputChange} >Call Language:
+                            <select name='callLanguage' value={callLanguage}>
+                                <option value="English">English</option>
+                                <option value="Español">Español</option>
+                                <option value="Русский">Русский</option>
+                            </select></Form.Label>
+                        <br></br>
+                        <Form.Control.Feedback type='invalid'></Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group>
@@ -116,7 +184,7 @@ const NewPostForm = () => {
                             value={postFormData.description}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>description is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>{!descriptionValidate ? "Please enter a description" : ""}</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group>
@@ -129,7 +197,7 @@ const NewPostForm = () => {
                             value={postFormData.callCategory}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>callCategory is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>{!callCategoryValidate ? "Please enter a category" : ""}</Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group>
@@ -141,7 +209,7 @@ const NewPostForm = () => {
                             value={postFormData.payment}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>payment is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'></Form.Control.Feedback>
                     </Form.Group>
 
 
@@ -156,7 +224,7 @@ const NewPostForm = () => {
                             value={postFormData.callTime}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>callTime is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>{!callTimeValidate ? "Please enter a task call time (please specify time zone)" : ""}</Form.Control.Feedback>
                     </Form.Group>
 
 
@@ -171,7 +239,7 @@ const NewPostForm = () => {
                             value={postFormData.phoneNumberToCall}
                             required
                         />
-                        <Form.Control.Feedback type='invalid'>phoneNumberToCall is required!</Form.Control.Feedback>
+                        <Form.Control.Feedback type='invalid'>{!phoneNumberToCallValidate ? "Please enter a phone number to call" : ""}</Form.Control.Feedback>
                     </Form.Group>
 
 
