02e5e05be651faa9f05da9292e59178c938a92c9
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-06-13 10:41:21 -0600

    changing path file

--- server/server.js
+++ server/server.js
@@ -23,12 +23,20 @@ app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
