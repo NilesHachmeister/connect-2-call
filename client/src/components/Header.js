6aa82a14c6e24e6581c22c067c2cc533a0b0129e
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-10-15 14:51:36 -0600

    resolving merge conflits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,13 +9,8 @@ import { Form } from 'react-bootstrap';
 import Home from './Home';
 
 import { useTranslation, Trans } from "react-i18next";
-<<<<<<< HEAD
 import { HashLink as Link } from 'react-router-hash-link';
 import { useNavigate } from 'react-router-dom';
-=======
-import { useNavigate } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
->>>>>>> a3c4a9be1316ea2c70ba53cbee74e5b5fa5704a0
 import "../i18n"
 import { t } from 'i18next';
 const lngs = {
@@ -88,13 +83,8 @@ function Header() {
           <Link to="/" style={inlineStyle}> {t("Home")}</Link>
           <Link to="/#homepage-signup" style={inlineStyle}>{t("Login")}</Link>
           <Link to="/signupform" style={inlineStyle}>  {t("Sign Up")}</Link>
-<<<<<<< HEAD
           <Link to={Auth.loggedIn() ? '/board' : '/'} style={inlineStyle}>{t("Board")}</Link>
           <Link to='/new-post' style={inlineStyle}>{t("New post")}</Link>
-=======
-          <Link to={Auth.loggedIn() ? '/board' : '/signupform'} style={inlineStyle}>{t("Board")}</Link>
-          <Link to={Auth.loggedIn() ? '/new-post' : '/signupform'} style={inlineStyle}>{t("New post")}</Link>
->>>>>>> a3c4a9be1316ea2c70ba53cbee74e5b5fa5704a0
 
           <br />
 
